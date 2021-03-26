ac8d09c6481af47a5e74f86bae0a710c2e6dccf1
Fa1th1 <f_faith@163.com>
2021-03-26 23:41:44 +0800

    GithubSession

--- src/main/java/com/hqu/hquforum/controller/AuthorizeController.java
+++ src/main/java/com/hqu/hquforum/controller/AuthorizeController.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import sun.net.httpserver.HttpServerImpl;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Drf
@@ -28,7 +31,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name ="code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -36,7 +40,13 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if (user != null){
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+            //登录成功，写入cookie和session
+        }else{
+            //登陆失败，重新登录
+            return "redirect:/";
+        }
     }
 }
